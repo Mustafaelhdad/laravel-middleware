007ae89dd12a798ec594e5a4f3b4a6accb81c9e8
Mustafaelhdad <Mostafa.eng_2194@eng.kfs.edu.eg>
2023-05-30 23:04:37 +0300

    middleware and seeders

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Run the database seeders.
+     */
+    public function run(): void
+    {
+        User::truncate();
+
+        $this->call([
+            UserSeeder::class,
+        ]);
+    }
+}
